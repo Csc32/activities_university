6185e50529ef01d91e5574f1a7393aebe03633c0
csc32 <sanzonettycar@gmail.com>
2024-05-20 05:00:08 -0400

    feat: add insert and show functionality

--- stack/index.php
+++ stack/index.php
@@ -13,15 +13,38 @@ class Stack
 
   public function insertElement()
   {
-    $el = readline("Ingrese un elemento: \n");
-    if (!$this->validateElement($el)) {
-      print("Ingrese un elemento valido");
-      return;
+    $counter = 1;
+    $totalLap = 1;
+    while (true) {
+      print("Vuelta numero: $totalLap \n");
+      if($totalLap == $this->finishLap){
+        $el = readline("Ingrese un elemento: \n");
+        if (!$this->validateElement($el)) {
+          print("\n Ingrese un elemento valido\n");
+        } else {
+          self::$stack[] = $el;
+        }
+        print("Proceso de insercion terminado total de vueltas: $totalLap \n");
+        break;
+      }
+      if ($counter !== $this->lap) {
+        $counter++;
+        $totalLap++;
+      } else {
+        $el = readline("Ingrese un elemento: \n");
+        if (!$this->validateElement($el)) {
+          print("\n Ingrese un elemento valido\n");
+        } else {
+          self::$stack[] = $el;
+        }
+        $counter = 1;
+        $totalLap++;
+      }
     }
   }
   private function validateElement($el): bool
   {
-    return isset($el) || strlen($el) == 0 ?  false : true;
+    return isset($el) && strlen($el) > 0;
   }
   public function remove()
   {
@@ -32,7 +55,15 @@ class Stack
 
     return $dataRemoved;
   }
+
+  public function show(){
+    return print_r(self::$stack);
+  }
 }
 
 $test = new Stack();
 $test->insertElement();
+$test->show();
+$test2 = new Stack(1, 100);
+$test2->insertElement();
+$test2->show();
